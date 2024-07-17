174094b399f5eed65c4d6b879a7f0f4f4b7986e6
noneotherthanmodi <uditmodi03@gmail.com>
2024-07-18 00:22:49 +0530

    Latest BSE news scrapper

--- scrap.py
+++ scrap.py
@@ -5,8 +5,20 @@ from urllib.parse import urlparse
 import asyncio
 import aiohttp
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from config import GEMINI_API_KEY
+import google.generativeai as genai
 
 base_url = "https://www.bseindia.com"
+
+response = requests.get("http://localhost:8080/corporate_announcements")
+output = json.loads(response.text)
+
+
+
+## SEND EACH PDF TO THE GEMINI AS SOON AS IT IS DOWNLOADED, SO THAT IT CAN BE PROCESSED IN PARALLEL
+
+
 
 
 async def download_pdf(pdf_url, local_filename):
@@ -18,6 +30,7 @@ async def download_pdf(pdf_url, local_filename):
                     if not chunk:
                         break
                     f.write(chunk)
+
 
 # Function to get text from PDFs
 def get_pdf_text(pdf_docs):
@@ -32,9 +45,21 @@ def get_pdf_text(pdf_docs):
 
 
 
-# Example code to fetch corporate announcements and process PDFs
-response = requests.get("http://localhost:8080/corporate_announcements")
-output = json.loads(response.text)
+
+#Function to split text into chunks
+def get_split_text(text):
+    text_splitter = CharacterTextSplitter(separator="\n",
+                                          chunk_size = 1000,
+                                          chunk_overlap=200,
+                                          length_function = len)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
+
+
+
 
 
 
@@ -61,7 +86,31 @@ for index, message in enumerate(output["message"]):
 
     # Store downloaded PDF path
     downloaded_pdf_paths.append(local_filename)
+    
+
 
 # Extract text from downloaded PDFs
-all_text = get_pdf_text(downloaded_pdf_paths)
-print(all_text)
+pdf_paths = downloaded_pdf_paths
+pdf_text = get_pdf_text(pdf_paths)
+
+
+
+pdf_split_text = get_split_text(pdf_text)
+pdf_split_text_string = ''.join(map(str, pdf_split_text))
+
+
+
+api_key = GEMINI_API_KEY
+os.environ["GOOGLE_API_KEY"] = api_key
+
+genai.configure(api_key=api_key)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+prompt = f"How did the company perform according to the financial result from this content :\n{pdf_split_text_string}"
+
+response = model.generate_content(prompt)
+
+with open("pdf_text.txt", "w",encoding='utf-8') as f:
+    f.write(response.text)
+
